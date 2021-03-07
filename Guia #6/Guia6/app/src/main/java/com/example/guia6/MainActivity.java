c35553126e9b2a58fb630bb49c836dfbb3c41031
ManuelGamez <gamezmanuel.progra17@gmail.com>
2021-03-07 14:59:52 -0600

    Guia#6

---
+++ Guia #6/Guia6/app/src/main/java/com/example/guia6/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.guia6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_constraint_layout);
+    }
+}
